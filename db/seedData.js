2b429649cf916b17330d093e8a79f627e9d165f6
Andy Nguyen <andyng1997@outlook.com>
2022-12-08 21:24:55 -0500

    Updated Seed Data

--- db/seedData.js
+++ db/seedData.js
@@ -5,12 +5,48 @@ const client = require("./client")
 async function dropTables() {
   console.log("Dropping All Tables...")
   // drop all tables, in the correct order
+  await client.query(`
+  DROP TABLE IF EXISTS routine_activities;
+  DROP TABLE IF EXISTS routines;
+  DROP TABLE IF EXISTS activities;
+  DROP TABLE IF EXISTS users;
+  `);
+  console.log("Finished dropping tables");
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
   // create all tables, in the correct order
+  await client.query(`
+  CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+  );
+  CREATE TABLE activities (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      description TEXT NOT NULL
+  );
+  CREATE TABLE routines (
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id),
+      "isPublic" BOOLEAN DEFAULT false,
+      name VARCHAR(255) UNIQUE NOT NULL,
+      goal TEXT NOT NULL
+  );
+  CREATE TABLE routine_activities (
+      id SERIAL PRIMARY KEY,
+      "routineId" INTEGER REFERENCES routines (id),
+      "activityId" INTEGER REFERENCES activities (id),
+      duration INTEGER,
+      count INTEGER,
+      UNIQUE ("routineId","activityId")
+  )  
+`);
+    console.log("Finishing creating tables")
 }
+
 
 /* 
 

51c70187cef4d16086aa2bf13b0e33e09d1f8c91
Marcus Mills Jr <marcus.mills97@yahoo.com>
2022-12-10 13:02:37 -0500

    feat: getAllActivities is up and running

--- db/activities.js
+++ db/activities.js
@@ -2,35 +2,55 @@ const client = require("./client")
 
 // database functions
 async function getAllActivities() {
-    const {rows: [activities]} = await client.query(`
+
+  try {
+    const {rows} = await client.query(`
       SELECT * 
       FROM activities;
-    `)
+    `);
 
-    console.log('activity rows', activities);
-    return activities
+    console.log('activity rows ------>>>>', rows);
+    return rows
+
+  } catch (error) {
+    console.log('error in getAllActivities -->>', error);
+    throw error
+  }
+  
 }
 
 async function getActivityById(id) {
-  const {rows: singleActivity} = await client.query(`
+
+  try {
+    const {rows: singleActivity} = await client.query(`
     SELECT *
     FROM activities 
-    WHERE id = ${id};
-  `)
+    WHERE "id" = ${id};
+  `);
 
-  console.log('Rows, getActivityById', );
+  console.log('Rows, getActivityById------->>>>>>>>>>' , singleActivity );
   return singleActivity
+  } catch (error) {
+    console.log('error in getActivityById', error);
+  }
+  
 }
 
 async function getActivityByName(name) {
-  const {rows: activityByName} = await client.query(`
+
+  try {
+    const {rows: activityByName} = await client.query(`
     SELECT *
     FROM activities 
-    WHERE "name" = ${name}
-  `)
+    WHERE "name" = ${name};
+  `);
 
-  console.log('activityByName', activityByName);
+  console.log('activityByName ----->>>>', activityByName);
   return activityByName
+  } catch (error) {
+    console.log('error in getActivityByName', error);
+  }
+ 
 }
 
 // select and return an array of all activities
@@ -41,7 +61,7 @@ async function attachActivitiesToRoutines(routines) {
         JOIN routines 
           ON activity.id 
         = routines.id;
-    `)
+    `);
 
     return rows
 }
@@ -49,6 +69,18 @@ async function attachActivitiesToRoutines(routines) {
 // return the new activity
 async function createActivity({ name, description }) {
 
+  try {
+    const {rows: activity} = await client.query(`
+      INSERT INTO activities (name,description)
+      VALUES ($1, $2)
+  ` , [name, description])
+
+  return activity
+  } catch (error) {
+    console.log('error in createActivity', error);
+    throw error
+  }
+  
 }
 
 // don't try to update the id

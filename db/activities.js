3ef9a8b7f6d08e6c817134bf841d97191d8f1082
Marcus Mills Jr <marcus.mills97@yahoo.com>
2022-12-10 11:57:33 -0500

    feat: added activity queries.. getAllActivities, getActivityById, getActivityByName, attachActivitiesToRoutines

--- db/activities.js
+++ db/activities.js
@@ -2,19 +2,48 @@ const client = require("./client")
 
 // database functions
 async function getAllActivities() {
+    const {rows: [activities]} = await client.query(`
+      SELECT * 
+      FROM activities;
+    `)
 
+    console.log('activity rows', activities);
+    return activities
 }
 
 async function getActivityById(id) {
-  
+  const {rows: singleActivity} = await client.query(`
+    SELECT *
+    FROM activities 
+    WHERE id = ${id};
+  `)
+
+  console.log('Rows, getActivityById', );
+  return singleActivity
 }
 
 async function getActivityByName(name) {
+  const {rows: activityByName} = await client.query(`
+    SELECT *
+    FROM activities 
+    WHERE "name" = ${name}
+  `)
 
+  console.log('activityByName', activityByName);
+  return activityByName
 }
 
 // select and return an array of all activities
 async function attachActivitiesToRoutines(routines) {
+    const {rows} = await client.query(`
+        SELECT *
+        FROM activities 
+        JOIN routines 
+          ON activity.id 
+        = routines.id;
+    `)
+
+    return rows
 }
 
 // return the new activity

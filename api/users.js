b9567752a76625900b4441c88fe01c43f662a1ad
Andy Nguyen <andyng1997@outlook.com>
2022-12-13 22:06:39 -0500

    Started api Users pt.1

--- api/users.js
+++ api/users.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+
+const { getAllUsers, createUser, getUserByUsername } = require('../db.users');
 
 // POST /api/users/login
+usersRouter.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+      next({
+        name: "Missing Username or Password",
+        message: "Please supply both a username and password"
+      });
+    }
+    const token = jwt.sign({ id: 1, username: 'albert' }, process.env.JWT_SECRET);
 
+    try {
+      const user = await getUserByUsername(username, password);
+      const SALT_COUNT = 10;
+      const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+  
+      if (!user && !user.password == hashedPassword) {
+        res.send({
+          name: 'IncorrectCredentialsError',
+          message: 'Username or password is incorrect'
+        });
+  
+      } else {
+        next({
+          message: "you're logged in!", token
+  
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
 // POST /api/users/register
 
 // GET /api/users/me

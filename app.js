26b365a354729d339b2718e11e14a501f4ff695a
Andy Nguyen <andyng1997@outlook.com>
2022-12-19 21:50:19 -0500

    got 6 out of 8 passing in api-activities

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -11,11 +10,25 @@ app.use(express.json());
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
-// Setup your Middleware and API Router here //
+// Setup your Middleware and API Router here
 app.get("/", function (req, res) {
   res.send({ msg: "Hello, I worked" });
 });
 
 app.use("/api", router);
+
+// Catch-all route
+app.use((req, res) => {
+  res.status(404).send({ message: "Resource not found" });
+});
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  res.status(500).send({
+    message: error.message,
+    name: error.name,
+    error: error.toString(),
+  });
+});
 
 module.exports = app;
